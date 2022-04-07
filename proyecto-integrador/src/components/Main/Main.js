10e57c431f28cfbb53617e1cf2ccc92a6a384bb1
jelizalde21 <63111904+jelizalde21@users.noreply.github.com>
2022-04-07 10:17:25 -0300

    add cards

--- proyecto-integrador/src/components/Main/Main.js
+++ proyecto-integrador/src/components/Main/Main.js
@@ -5,15 +5,16 @@ class Main extends Component {
     constructor(props){
         super(props)
         this.state = {
-            value:'',
+            value: 1,
             peliculas:[]
         }
     }
     componentDidMount(){
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=a86dfc3e8792d8c35b2b0bcfae9f4488&language=en-US&page=1')
+        fetch( `https://api.themoviedb.org/3/movie/popular?api_key=a86dfc3e8792d8c35b2b0bcfae9f4488&language=en-US&page=${this.state.value}` )
         .then((response)=>response.json())
         .then((data)=> this.setState({
-            peliculas: data.results
+            peliculas: data.results,
+            value: this.state.value + 1
         }))
         .catch((e)=>console.log(e))
     }
@@ -23,12 +24,23 @@ class Main extends Component {
             peliculas: resultado
         })
     }
+    agregarPelicula(){
+        fetch( `https://api.themoviedb.org/3/movie/popular?api_key=a86dfc3e8792d8c35b2b0bcfae9f4488&language=en-US&page=${this.state.value}` )
+        .then((response)=>response.json())
+        .then((data) => {
+            let arrayViejo = this.state.peliculas
+            this.setState({
+                peliculas: arrayViejo.concat(data.results),
+                value: this.state.value + 1
+            })
+        } )
+    }
     render() {
         return (
             this.state.peliculas.length===0?<img className='spinner' src='./images/spiner.gif' alt='spinner'/>:
 
         <main>
-            <button type="button">Cargar más tarjetas</button>
+            <button type="button" onClick={()=>this.agregarPelicula()}>Cargar más tarjetas</button>
             <section className="card-container">
                 {this.state.peliculas.map((pelicula)=><Article borrar={(id)=> this.borrarPelicula(id)} key={pelicula.id + Date.now()} info={pelicula}/>)}
             </section>
