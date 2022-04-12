a8c6d3e0c5fb471fddca3a457c0e8d05143978d3
jelizalde21 <63111904+jelizalde21@users.noreply.github.com>
2022-04-12 14:11:11 -0300

    Commit

--- proyecto-integrador/src/components/Header/Header.js
+++ proyecto-integrador/src/components/Header/Header.js
@@ -18,16 +18,18 @@ class Header extends Component {
   render() {
     return (
         <header>
-        <h1>Flickz (nombre provisorio)</h1>
-        <section>
-            <i className="fas fa-th"></i>
-            <i className="fas fa-align-justify"></i>
-            <form onSubmit={(event)=> this.evitarSubmit(event)} action="">
-                <input onChange={(event)=>this.controlarCambio(event)}  type="text" name="search" id="" placeholder="Search"/>
-                <button type="submit"><i className="fas fa-search"></i></button>
-            </form>
-        </section>
-    </header>
+            <section className='section1'>
+                <h1>Flickz (nombre provisorio)</h1>
+            </section>
+            <section className='section2'>
+                <i className="fas fa-th"></i>
+                <i className="fas fa-align-justify"></i>
+                <form onSubmit={(event)=> this.evitarSubmit(event)} action="">
+                    <input onChange={(event)=>this.controlarCambio(event)}  type="text" name="search" id="" placeholder="Search"/>
+                    <button type="submit"><i className="fas fa-search"></i></button>
+                </form>
+            </section>
+        </header>
     )
   }
 }
