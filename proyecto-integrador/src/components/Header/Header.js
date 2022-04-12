32ded1b1384a9460716958a312962399e8ff91b7
jelizalde21 <63111904+jelizalde21@users.noreply.github.com>
2022-04-12 16:42:18 -0300

    Merge y demás

--- proyecto-integrador/src/components/Header/Header.js
+++ proyecto-integrador/src/components/Header/Header.js
@@ -18,31 +18,18 @@ class Header extends Component {
   render() {
     return (
         <header>
-<<<<<<< HEAD
             <section className='section1'>
-                <h1>Flickz (nombre provisorio)</h1>
+                <h1>Flickz</h1>
             </section>
             <section className='section2'>
-                <i className="fas fa-th"></i>
-                <i className="fas fa-align-justify"></i>
+                <i onClick={()=>this.props.visualVertical()} className="fas fa-th"></i>
+                <i onClick={()=>this.props.visualHorizontal()} className="fas fa-align-justify"></i>
                 <form onSubmit={(event)=> this.evitarSubmit(event)} action="">
-                    <input onChange={(event)=>this.controlarCambio(event)}  type="text" name="search" id="" placeholder="Search"/>
+                    <input onChange={(event)=>this.controlarCambio(event)} value={this.state.cambio} type="text" name="search" id="" placeholder="Search"/>
                     <button type="submit"><i className="fas fa-search"></i></button>
                 </form>
             </section>
-        </header>
-=======
-        <h1>Flickz</h1>
-        <section>
-            <i onClick={()=>this.props.visualVertical()} className="fas fa-th"></i>
-            <i onClick={()=>this.props.visualHorizontal()} className="fas fa-align-justify"></i>
-            <form onSubmit={(event)=> this.evitarSubmit(event)} action="">
-                <input onChange={(event)=>this.controlarCambio(event)} value={this.state.cambio} type="text" name="search" id="" placeholder="Search"/>
-                <button type="submit"><i className="fas fa-search"></i></button>
-            </form>
-        </section>
     </header>
->>>>>>> 5e6545dcc54b47e351ec481660a52aa9f820c37a
     )
   }
 }
